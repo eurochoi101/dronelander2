c04b99823f250f3beeea89ae2d335ebb00ed788d
Joseph Stone <josephstone@Josephs-MacBook-Pro.local>
2017-10-10 14:34:31 -0600

    init comm

---
+++ DroneLander/DroneLander.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DroneLander.Droid
+{
+    [Activity(Label = "DroneLander", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            MobileCenter.Start("b1987ef4-3630-4f23-b1d5-9096965bff05",
+                   typeof(Analytics), typeof(Crashes));
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
